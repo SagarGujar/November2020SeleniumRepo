8d9193a6556f6ac8bab1933df05a3d61f8a0ad9a
SagarGujar <2020mve010@sggs.ac.in>
2021-05-30 19:08:01 +0530

    add just a space

--- November2020_7AMSeleniumBatch/src/com/launchings/TC_006.java
+++ November2020_7AMSeleniumBatch/src/com/launchings/TC_006.java
@@ -3,8 +3,8 @@ package com.launchings;
 public class TC_006 {
 
 	public static void main(String[] args) {
-		System.out.println("first commit");
-		System.out.println("second commit");
+		System.out.println("first  commit");
+		System.out.println("second  commit");
 
 	}
 
